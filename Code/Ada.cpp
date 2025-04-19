a5e22a949123c28bbf7c3ca1365da1f1a55f9945
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-04-18 21:15:18 -0400

    x

--- Code/Ada.cpp
+++ Code/Ada.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <string>
 #include <unordered_map>
+//DONT USE .h in COMPILE COMMAND
 // use rm Code/Ada.exe to recompile
 // then g++ -O3 Code/Ada.cpp -o Ada.exe
 int main()
