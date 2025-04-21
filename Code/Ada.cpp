f7402ee269937a505c19e8e5253fe4d12e8ee42c
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-04-21 00:14:52 -0400

    Added a test for softmax batch, used safer methods.

--- Code/Ada.cpp
+++ Code/Ada.cpp
@@ -12,8 +12,21 @@
 //  then g++ -O3 Code/Ada.cpp -o Ada.exe
 int main()
 {
- 
-    
+    // example rows
+    std::vector<std::vector<float>> inputs = {
+        {0.0f, 1.2f},
+        {3.5f, 2.0f}};
+    // loop through the test inputs, softmaxing each
+    auto result = softmaxBatch(inputs);
+    // loop through the rows, prints results, then ending line after each row
+    for (const auto &val : result)
+    {
+        for (const auto &i : val)
+        {
+            std::cout << i << "\t";
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
