f1cd4b05b4d18759c60ec8a623421ac96ab7d51f
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-04-22 02:37:52 -0400

    x

--- Code/Ada.cpp
+++ Code/Ada.cpp
@@ -3,12 +3,6 @@
 #include <vector>
 #include "Adamath.h"
 #include "AiComputations.h"
-#include <sstream>
-#include <fstream>
-#include <string>
-#include <unordered_map>
-#include <chrono>
-#include <random>
 // DONT USE .h in COMPILE COMMAND
 //  use rm Ada.exe to remove original .exe file if present, or EXAMPLEFOLDER/Ada.exe
 //  then g++ -O3 Code/Ada.cpp -o Ada.exe
