014c57315ce73fe6ea3d14d9bd8cba25836664bd
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-04-22 19:51:27 -0400

    edited example code for matrix math, changed the 2d vector result to auto result.

--- Code/Ada.cpp
+++ Code/Ada.cpp
@@ -10,6 +10,21 @@
 int main()
 {
 
+    std::vector<std::vector<float>> A = {
+        {0.5, 0.3, 0.2},
+        {0.2, 0.6,0.2},
+        {0.1, 0.4, 0.5}
+    };
+
+    std::vector<std::vector<float>> B = {
+        {2, 1},
+        {0, 3},
+        {1, 2}
+    };
+
+    auto result = matmul(A, B);
+
+    printMatrix(result);
   
     return 0;
 }
--- Code/Adamath.h
+++ Code/Adamath.h
@@ -134,17 +134,18 @@ std::vector<std::vector<float>> matmul(const std::vector<std::vector<float>> &A,
 // example running of matrix multiply code:
 /*
      std::vector<std::vector<float>> A = {
-        {1, 2, 3},
-        {4, 5, 6}
+        {0.5, 0.3, 0.2},
+        {0.2, 0.6,0.2},
+        {0.1, 0.4, 0.5}
     };
 
     std::vector<std::vector<float>> B = {
-        {7, 8},
-        {9, 10},
-        {11, 12}
+        {2, 1},
+        {0, 3},
+        {1, 2}
     };
 
-    std::vector<std::vector<float>> result = matmul(A, B);
+    auto result = matmul(A, B);
 
     printMatrix(result);
 
